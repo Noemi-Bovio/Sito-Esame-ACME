8554940682a02c716f3e38ab7fa0dadefeb58584
Noemi-Bovio <98837429+Noemi-Bovio@users.noreply.github.com>
2022-06-30 17:26:01 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,3 +2,13 @@
 function scrollToTop() {
            window.scrollTo(0, 0);
         }
+
+//cookie
+function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
+function hideCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:none;";
+    }
